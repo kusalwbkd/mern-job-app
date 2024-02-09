c2e61131543d81d013322e32d7586ded96ae2ccb
kuslwbkd <kusalwbkddhananjaya@gmail.com>
2024-02-09 11:57:30 +0530

    first commit

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,30 @@
+
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Link } from 'react-router-dom';
+import {Logo,FormRow} from '../components';
+
+const Register = () => {
+  return (
+    <Wrapper>
+      <form className='form'>
+      <Logo />
+        <h4>Register</h4>
+      
+        <FormRow type='text' name='name' />
+        <FormRow type='text' name='lastName' labelText='last name' />
+        <FormRow type='text' name='location' />
+        <FormRow type='email' name='email' />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+export default Register
